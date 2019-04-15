7080996291f5a7bcd7b8edca0d66e66f93d32e16
Slardar <me25show@gmail.com>
2019-04-15 14:41:35 +1000

    Handel share

--- app/src/main/java/net/slardar/noadsyoutube/SplashScreen.kt
+++ app/src/main/java/net/slardar/noadsyoutube/SplashScreen.kt
@@ -1,12 +1,32 @@
 package net.slardar.noadsyoutube
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var launchYouTube: TextView
+    private lateinit var acceptButton: Button
+    private var shareURL: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        launchYouTube = findViewById(R.id.launch_YouTube)
+        acceptButton = findViewById(R.id.accept_button)
+
+        //Set Listener
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val intent = intent
+        if (intent.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+            shareURL = intent.getStringExtra(Intent.EXTRA_TEXT)
+        } else {
+            shareURL = ""
+        }
     }
 }
