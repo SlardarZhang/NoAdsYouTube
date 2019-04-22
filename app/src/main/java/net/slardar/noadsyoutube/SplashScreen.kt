3472d39e4628cb339bc1b0bf435e1e0fe75cb293
Slardar <me25show@gmail.com>
2019-04-23 07:55:37 +1000

    Allow Browser play & Allow backup

--- app/src/main/java/net/slardar/noadsyoutube/SplashScreen.kt
+++ app/src/main/java/net/slardar/noadsyoutube/SplashScreen.kt
@@ -10,7 +10,7 @@ import android.widget.TextView
 class SplashScreen : AppCompatActivity() {
     private lateinit var launchYouTube: TextView
     private lateinit var acceptButton: Button
-    private var shareURL: String = ""
+    private var videoID: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +20,23 @@ class SplashScreen : AppCompatActivity() {
 
         //Set Listener
         launchYouTube.setOnClickListener {
-            var intent = packageManager.getLaunchIntentForPackage("com.google.android.youtube")
-            if (intent != null) {
+            var intent: Intent? = packageManager.getLaunchIntentForPackage("com.google.android.youtube")
+            when (intent != null) {
                 // Set Flags to start YouTube
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            } else {
+                true -> {
+                    when (videoID.isNotEmpty()) {
+                        true -> {
+                            intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + videoID))
+                        }
+                    }
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+
                 // Launch Google Play Store
-                intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri.parse(getString(R.string.youtube_market))
+                false -> {
+                    intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse(getString(R.string.youtube_market))
+                }
             }
             startActivity(intent)
         }
@@ -35,8 +44,8 @@ class SplashScreen : AppCompatActivity() {
         acceptButton.setOnClickListener {
             val intent = Intent(baseContext, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-            if (shareURL.isNotEmpty()) {
-                intent.putExtra("shareURL", shareURL)
+            if (videoID.isNotEmpty()) {
+                intent.putExtra("shareURL", videoID)
             }
             startActivity(intent)
             finish()
@@ -46,10 +55,46 @@ class SplashScreen : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val intent: Intent = intent
-        if (intent.action == Intent.ACTION_SEND && intent.type == "text/plain") {
-            shareURL = intent.getStringExtra(Intent.EXTRA_TEXT)
-        } else {
-            shareURL = ""
+
+        when (intent.action) {
+            Intent.ACTION_SEND -> {
+                videoID = if (intent.type == "text/plain") {
+                    intent.getStringExtra(Intent.EXTRA_TEXT)
+                } else {
+                    ""
+                }
+                if (videoID.isNotBlank()) {
+                    videoID = if (videoID.lastIndexOf("/") != -1) {
+                        videoID.substring(videoID.lastIndexOf("/") + 1)
+                    } else {
+                        ""
+                    }
+                }
+            }
+
+            Intent.ACTION_MAIN -> {
+                videoID = ""
+            }
+
+            Intent.ACTION_VIEW -> {
+                videoID = if (intent.data != null) {
+                    intent.data!!.toString()
+                } else {
+                    ""
+                }
+                if (videoID.isNotBlank()) {
+                    videoID = if (videoID.lastIndexOf("?v=") != -1) {
+                        videoID.substring(videoID.lastIndexOf("?v=") + 3)
+                    } else {
+                        ""
+                    }
+                }
+            }
+
+            else -> {
+                videoID = ""
+            }
         }
+
     }
 }
